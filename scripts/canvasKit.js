539c3937dba37f2bffb8198b9d8c418ac053c483
monkeywithoutthee <54135971+monkeywithoutthee@users.noreply.github.com>
2022-05-14 09:58:02 +0100

    Add files via upload

---
+++ scripts/canvasKit.js
@@ -0,0 +1,263 @@
+// get references to the canvas and context
+const canvas = document.getElementById("canvas");
+const overlay = document.getElementById("overlay");
+const ctx = canvas.getContext("2d");
+const ctxo = overlay.getContext("2d");
+const canBackground = document.getElementById('canBack');
+const ctxb = canBackground.getContext('2d');
+
+
+
+// calculate where the canvas is on tzhe window
+var x = canvas.getClientRects();
+console.log(x,'<<canvas');
+var oPage = {
+  options:['square','circle','grade'],
+  currentOption:'square',
+  offsetX:x[0].left,
+  offsetY:x[0].top,
+  isDown:false,
+  startX:null,
+  startY:null,
+  prevStartX:0,
+  prevStartY:0,
+  prevWidth:0,
+  prevHeight:0,
+  mouseX:0,
+  mouseY:0,
+  lastPoint:{x:null,y:null},
+  isPathStarted:false,
+  selColors:['#ffff00','#ff0000','#00ff00'],
+  images:[],
+  currentImage:0
+};
+//image will have layers, layers will have data, data will differ on shape
+/*images[
+  {
+  imagedata:[
+  {
+  type:'square',
+  move:{}
+  }
+            ]
+  }
+]
+
+
+]*/
+//var index = oPage.options.indexOf(oPage.currentOption);
+//document.getElementById('colPickr').value = oPage.selColors[oPage.options.indexOf(oPage.currentOption)];
+// style the context
+ctx.strokeStyle = oPage.selColors[oPage.options.indexOf(oPage.currentOption)];
+ctx.lineWidth = 3;
+ctxo.strokeStyle = oPage.selColors[oPage.options.indexOf(oPage.currentOption)];
+ctxo.lineWidth = 2;
+const handleMouseDown = ((event)=>{
+    event.preventDefault();
+    event.stopPropagation();
+  //  ctxo.font = '48px serif';
+  //  ctxo.strokeText('booom', 10, 50);
+    // save the starting x/y of the rectangle
+    oPage.startX = parseInt(event.clientX - oPage.offsetX);
+    oPage.startY = parseInt(event.clientY - oPage.offsetY);
+    if (event.shiftKey){
+      oPage.isPathStarted = false;
+      return;
+    };
+    if (oPage.currentOption==='grade'){
+      if (!oPage.isDown){
+        ctxo.beginPath();
+      };
+      if (oPage.isPathStarted){
+        oPage.images[oPage.currentImage].imagedata.push({type:'grade',move:{x:oPage.lastPoint.x,y:oPage.lastPoint.y}});
+        ctxo.lineTo(oPage.lastPoint.x,oPage.lastPoint.y);
+      };
+      oPage.images[oPage.currentImage].imagedata.push({type:'grade',move:{x:oPage.startX,y:oPage.startY}});
+      ctxo.lineTo(oPage.startX,oPage.startY);
+      ctxo.stroke();
+      oPage.lastPoint.x = oPage.startX;
+      oPage.lastPoint.y = oPage.startY;
+      oPage.isPathStarted = true;
+     console.log('in grade DOWN::',oPage.images[oPage.currentImage])
+    };
+
+    oPage.isDown = true;
+
+  console.log('mouse down start::',oPage.images[oPage.currentImage]);
+    return;
+});
+
+const handleMouseUp = ((event)=>{
+    event.preventDefault();
+    event.stopPropagation();
+    // the drag is over, clear the dragging flag
+    oPage.startX = parseInt(event.clientX - oPage.offsetX);
+    oPage.startY = parseInt(event.clientY - oPage.offsetY);
+
+    if (oPage.currentOption==='square'){
+      ctx.strokeStyle = oPage.selColors[0];
+      ctxo.strokeStyle = oPage.selColors[0];
+      //add image data (type and move);
+      oPage.images[oPage.currentImage].imagedata.push({type:'square',move:{x:oPage.prevStartX,y:oPage.prevStartY,width:oPage.prevWidth,height:oPage.prevHeight}});
+      ctxo.strokeRect(oPage.prevStartX, oPage.prevStartY, oPage.prevWidth, oPage.prevHeight);
+    };
+    if (oPage.currentOption==='circle'){
+      ctx.strokeStyle = oPage.selColors[1];
+      ctxo.strokeStyle = oPage.selColors[1];
+      if (oPage.prevWidth/2>1){
+                oPage.images[oPage.currentImage].imagedata.push({type:'circle',move:{x:oPage.prevStartX *1+(oPage.prevWidth/2),y:oPage.prevStartY *1+(oPage.prevWidth/2),radius:oPage.prevWidth/2,starta:0,enda:360}});
+        ctxo.beginPath();
+        ctxo.arc(oPage.prevStartX *1+(oPage.prevWidth/2), oPage.prevStartY *1+(oPage.prevWidth/2), oPage.prevWidth/2, 0, 360);
+        ctxo.stroke();
+      };
+    //  console.log(oPage.prevStartX,oPage.prevStartY,'<<drawing circle MUP!:',oPage.prevWidth,oPage.prevHeight);
+    };
+   if (oPage.currentOption==='grade'){
+     ctx.strokeStyle = oPage.selColors[2];
+     ctxo.strokeStyle = oPage.selColors[2];
+     //note - adding image data on multi shape is done on mousedown
+
+      oPage.lastPoint.x = oPage.startX;
+      oPage.lastPoint.y = oPage.startY;
+    //  console.log('in mouseup grade');
+    };
+    oPage.isDown = false;
+});
+
+const handleMouseOut = ((event)=>{
+    event.preventDefault();
+    event.stopPropagation();
+    // the drag is over, clear the dragging flag
+    oPage.isDown = false;
+});
+
+const handleMouseMove = ((event)=>{
+    event.preventDefault();
+    event.stopPropagation();
+
+    // if we're not dragging, just return
+    if (!oPage.isDown&&oPage.currentOption!=='grade') {
+       return;
+    }
+    // get the current mouse position
+    oPage.mouseX = parseInt(event.clientX - oPage.offsetX);
+    oPage.mouseY = parseInt(event.clientY - oPage.offsetY);
+    // calculate the rectangle width/height based
+    // on starting vs current mouse position
+    const width = oPage.mouseX - oPage.startX;
+    const height = oPage.mouseY - oPage.startY;
+
+    // clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // draw a new rect from the start position
+    // to the current mouse position
+    if (oPage.currentOption==='square'){
+      ctx.borderRadius = "50";
+      ctx.strokeRect(oPage.startX, oPage.startY, width, height);
+    };
+    if (oPage.currentOption==='circle'){
+      if (width>1){
+        ctx.beginPath();
+        ctx.arc(oPage.startX*1+(width/2), oPage.startY*1+(width/2), width/2, 0, 360);
+        ctx.stroke();
+      };
+    };
+    if (oPage.currentOption==='grade'&&oPage.isPathStarted){
+      //ctx.lineTo(oPage.mouseX-50, oPage.mouseY-50);
+      ctx.beginPath();
+      ctx.lineTo(oPage.lastPoint.x,oPage.lastPoint.y);
+      ctx.lineTo(oPage.mouseX, oPage.mouseY);
+      ctx.stroke();
+      //console.log(oPage.startX,oPage.startY,'<<drawing grade!::',oPage.mouseX,oPage.mouseY);
+    };
+    oPage.prevWidth  = width;
+    oPage.prevHeight = height;
+    oPage.prevStartX = oPage.startX;
+    oPage.prevStartY = oPage.startY;
+});
+const removeClass = ((data)=>{
+    //data = {class:''};
+    var o = document.querySelectorAll('.'+data.class);
+    if (o){
+      o.forEach((item, i)=>{
+        item.classList.remove(data.class);
+      });
+    };
+});
+canvas.addEventListener('mousedown',(event)=>{
+  handleMouseDown(event);
+});
+canvas.addEventListener('mousemove',(event)=>{
+  handleMouseMove(event);
+});
+canvas.addEventListener('mouseup',(event)=>{
+  handleMouseUp(event);
+});
+canvas.addEventListener('mouseout',(event)=>{
+  handleMouseOut(event);
+});
+document.addEventListener('click',(event)=>{
+  //universal click filtered by class?
+  //console.log('clicking::',event);
+  const evTar = event.target;
+  if (evTar.className.includes('optionEl')){
+    removeClass({class:'optionElHigh'});
+    if (evTar.innerText==='square'){
+      oPage.currentOption = 'square';
+    };
+    if (evTar.innerText==='circle'){
+      oPage.currentOption = 'circle';
+    };
+    if (evTar.innerText==='grade'){
+      oPage.currentOption = 'grade';
+    };
+    evTar.classList.add('optionElHigh');
+    oPage.isDown = false;
+    oPage.isPathStarted = false;
+    const index = oPage.options.indexOf(oPage.currentOption);
+    document.getElementById('colPickr').value = oPage.selColors[index];
+    ctx.strokeStyle = oPage.selColors[index];
+    ctxo.strokeStyle = oPage.selColors[index];
+    //console.log(oPage.currentOption,'<<in optionEl SQ');
+    return;
+  };
+});
+document.getElementById('colPickr').addEventListener('change',(event)=>{
+  changeOptionCol({value:event.target.value});
+  console.log('colPickr::',event.target.value);
+});
+document.addEventListener('DOMContentLoaded',(event)=>{
+  //open the last image OR start a new one
+  if (oPage.images.length===0){
+    oPage.images.push({imagedata:[{type:oPage.currentOption,moves:[]}]})
+  }else{
+    //else draw last image
+    drawstuff();
+  };
+  //optionElHigh
+  var opt = document.querySelectorAll('.optionEl');
+  var index = oPage.options.indexOf(oPage.currentOption);
+  //optionElHigh
+  opt[index].firstChild.classList.add('optionElHigh');
+    console.log(oPage.images,'<<DOMContentLoaded::',opt[index],index);
+});
+const changeOptionCol = ((data)=>{
+  //data = {value:''};
+  const index = Array.from(oPage.options).indexOf(oPage.currentOption);
+  oPage.selColors[index] = data.value;
+  ctxo.strokeStyle = oPage.selColors[index];
+  ctx.strokeStyle = oPage.selColors[index];
+  console.log(data,'<<',index,oPage.selColors);
+
+});
+const drawstuff = (()=>{
+  console.log('drawstuff::', oPage.images);
+/*  ctx.clearRect(0,0,400,400);
+  for (var i = 0; i < oPage.data.length; i++){
+    ctx.beginPath();
+    oPage.data[i].forEach((item, i) => {
+      ctx.lineTo(item.x, item.y);
+    });
+    ctx.stroke();
+  };*/
+});
